e4d4f1e6057c427bc5dc004a404b29f52f669b57
Daniel <daniel.greenfeld@britecore.com>
2018-10-10 11:56:04 -0700

    Add BriteCore as sponsor

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "0.5.1"
+version = "0.5.3"
 
 if sys.argv[-1] == "publish":
     try:
--- whichcraft.py
+++ whichcraft.py
@@ -2,7 +2,7 @@
 
 __author__ = "Daniel Roy Greenfeld"
 __email__ = "pydanny@gmail.com"
-__version__ = "0.5.1"
+__version__ = "0.5.3"
 
 import os
 import sys
