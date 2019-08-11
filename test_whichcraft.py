f7b6d13f1a13413c4d80f32b6e3c67650c9f1b1d
Nooby-Newbie <40718988+Nooby-Newbie@users.noreply.github.com>
2019-08-11 12:14:53 +0000

    Update test_whichcraft.py

--- test_whichcraft.py
+++ test_whichcraft.py
@@ -1,10 +1,11 @@
 import os
-
+from datetime import date
 import pytest
 from whichcraft import which
+import sys
 
-
-def test_existing_command():
+@pytest.mark.skipif(sys.platform == "win32", reason= "Does not run on windows")
+def test_existing_linux():
     cmd = which("date")
     assert cmd
     assert os.path.exists(cmd)
@@ -14,6 +15,14 @@ def test_existing_command():
 
 def test_non_existing_command():
     assert which("stringthatisntashellcommand") is None
+
+@pytest.mark.skipif(sys.platform != "win32", reason= "Does not run on windows")
+def test_existing_windows():
+    cmd = which("cmd")
+    assert cmd
+    assert os.path.exists(cmd)
+    assert os.access(cmd, os.F_OK | os.X_OK)
+    assert not os.path.isdir(cmd)
 
 
 if __name__ == "__main__":
